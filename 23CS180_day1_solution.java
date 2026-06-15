e5fbb3b77c822f098c6e5182bd3ec30a24af8069
VAISHNUPRIYA-V <vaishnupriya0811@gmail.com>
2026-06-15 21:08:35 +0530

    Program_5

--- 23CS180_day1_solution.java
+++ 23CS180_day1_solution.java
@@ -37,10 +37,19 @@ class day1_solution{
 
     }
 
+    public static void program5() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("PROGRAM 5");
+    System.out.println("Enter a number:");
+    int n = sc.nextInt();
+    System.out.println("Binary Number:" + Integer.toBinaryString(n));
+    }
+
     public static void main(String[] args) {
         //program1();
         //program2();
         //program3();
-        program4();
+        //program4();
+        program5();
     }
 }
