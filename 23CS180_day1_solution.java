63860f3b240299f922df22e0dddc95b176e89252
VAISHNUPRIYA-V <vaishnupriya0811@gmail.com>
2026-06-15 21:26:16 +0530

    Program_8

--- 23CS180_day1_solution.java
+++ 23CS180_day1_solution.java
@@ -55,12 +55,29 @@ class day1_solution{
     }
 
     public static void program7() {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("PROGRAM 7");
-    System.out.println("Enter a number:");
-    int n = sc.nextInt();
-    String octal = Integer.toOctalString(n);
-    System.out.println("Octal:" + octal);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("PROGRAM 7");
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        String octal = Integer.toOctalString(n);
+        System.out.println("Octal:" + octal);
+    }
+    public static void program8() {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a binary number: ");
+        String binary = sc.next();
+
+        int decimal = 0;
+        int power = 0;
+
+        for (int i = binary.length() - 1; i >= 0; i--) {
+            int bit = binary.charAt(i) - '0';
+            decimal += bit * Math.pow(2, power);
+            power++;
+        }
+        System.out.println("Decimal number is : " + decimal);
+        
     }
 
     public static void main(String[] args) {
@@ -70,6 +87,7 @@ class day1_solution{
         //program4();
         //program5();
         //program6();
-        program7();
+        //program7();
+        program8();
     }
 }
