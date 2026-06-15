53d407b5eb3f5aa850b0bfa0a9c9920e9b928271
VAISHNUPRIYA-V <vaishnupriya0811@gmail.com>
2026-06-15 20:52:05 +0530

    Program_2

--- 23CS180_day1_solution.java
+++ 23CS180_day1_solution.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
-class 23CS180_day1_solution{
-    public static void main(String[] args) {
+class day1_solution{
+    public static void program2() {
+        System.out.println("PROGRAM 2");
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+
+    public static void program1() {
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
         System.out.println("Hello"+" "+s);
+    }
+    public static void main(String[] args) {
+        //program1();
+        program2();
     }
 }
